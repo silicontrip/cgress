a9580242fff8f1e6c1905ba88fa80dd72ef1cd0f
Mark Heath <mheath@NGTV41293.local>
2025-01-24 12:35:42 +1100

    Added option to show the cyclone edge in different colours.

--- cyclonefields.cpp
+++ cyclonefields.cpp
@@ -24,23 +24,26 @@ class cyclonefields {
 private:
 	unordered_map<field,int> mucache;
 	draw_tools dt;
+	string plan_colour;
+	string edge_colour;
 	run_timer rt;
 	int calculation_type;
 	vector<field> all;
 	int sameSize;
 	bool splits;
+	bool edge_path;
 
 	int cached_mu (field f);
-	string draw_fields(const vector<field>& f);
+	string draw_fields(const vector<field>& f, const vector<line>& l);
 	int iterate_search(int start, line medge, vector<field>fields_list, int max, point third_point);
 	int count_links (point p, vector<field> flist);
 	vector<field> get_cadence(field outer, line edge, int start);
 	line get_edge(vector<field>plan, field newfield);
-	int next_cadence(int i, line newedge, vector<field>fields_list, field newfield, int max);
+	int next_cadence(int i, line newedge, vector<field>fields_list, field newfield, int max, vector<line>cyc_edges);
 
 
 public:
-	cyclonefields(draw_tools dts, run_timer rtm, int calc, bool s, const vector<field> a);
+	cyclonefields(draw_tools dts, draw_tools edt, run_timer rtm, int calc, bool s, bool e, const vector<field> a);
 	void search_fields();
 
 };
@@ -109,13 +112,13 @@ vector<field> cyclonefields::get_cadence (field outer, line edge, int start)
 	return cad_fields;
 }
 
-int cyclonefields::next_cadence(int i, line newedge, vector<field>fields_list, field newfield, int max)
+int cyclonefields::next_cadence(int i, line newedge, vector<field>fields_list, field newfield, int max, vector<line>cyc_edges)
 {
 	fields_list.push_back(newfield);
 	if (fields_list.size() > max) {
 		max = fields_list.size();
 		// Draw tools
-		cout << max << " : " << draw_fields(fields_list) << endl;
+		cout << max << " : " << draw_fields(fields_list,cyc_edges) << endl;
 		cerr << rt.split() << " seconds." << endl;
 	}
 
@@ -123,7 +126,8 @@ int cyclonefields::next_cadence(int i, line newedge, vector<field>fields_list, f
 	for (field cf2 : inner_cad)
 	{		
 		line newedge = get_edge(fields_list,cf2);
-		max = next_cadence(i, newedge, fields_list, cf2, max);
+		cyc_edges.push_back(newedge);
+		max = next_cadence(i, newedge, fields_list, cf2, max,cyc_edges);
 	}
 	return max;
 }
@@ -131,10 +135,12 @@ int cyclonefields::next_cadence(int i, line newedge, vector<field>fields_list, f
 void cyclonefields::search_fields()
 {
 	int max = 1;
+	vector<line>cyclone_edges;
 	for (int i = 0; i < all.size(); i++) {
 		field this_field = all[i];
 		vector<line> edges = this_field.get_lines();
 		for (line edge : edges) {
+
 			vector<field> cad_fields = get_cadence(this_field, edge, i+1);
 
 
@@ -146,6 +152,9 @@ void cyclonefields::search_fields()
 				vector<line> medges = cfi.get_lines();
 				for (line medge : medges) {
 					if (!(medge == edge)) {
+						cyclone_edges.erase(cyclone_edges.begin(),cyclone_edges.end());
+						cyclone_edges.push_back(edge);
+						cyclone_edges.push_back(medge);
 
 						vector<field> inner_cad = get_cadence(cfi, medge, i+1);
 
@@ -157,10 +166,11 @@ void cyclonefields::search_fields()
 							fields_list.push_back(cfi);
 
 							line newedge = get_edge(fields_list,cf2);
+							cyclone_edges.push_back(newedge);
 
 							//fields_list.push_back(cf2);
 
-							max = next_cadence(i+1, newedge, fields_list, cf2, max);
+							max = next_cadence(i+1, newedge, fields_list, cf2, max,cyclone_edges);
 
 							// we should be able to implicitely determine the cadence 
 							// and therefore the next edge
@@ -176,13 +186,15 @@ void cyclonefields::search_fields()
 }
 
 
-
-cyclonefields::cyclonefields(draw_tools dts, run_timer rtm, int calc, bool s, const vector<field> a)
+cyclonefields::cyclonefields(draw_tools dts, draw_tools edt, run_timer rtm, int calc, bool s, bool e, const vector<field> a)
 {
 	dt = dts;
 	rt = rtm;
 	calculation_type = calc;
 	splits = s;
+	edge_path = e;
+	plan_colour = dts.get_colour();
+	edge_colour = edt.get_colour();
 	// this can be a shallow copy.  is this right?
 	all = a;
 }
@@ -196,7 +208,7 @@ int cyclonefields::cached_mu (field f)
 	return mucache[f];
 }
 
-string cyclonefields::draw_fields(const vector<field>& f)
+string cyclonefields::draw_fields(const vector<field>& f, const vector<line>& l)
 {
 
 	dt.erase();
@@ -210,6 +222,14 @@ string cyclonefields::draw_fields(const vector<field>& f)
 	} else {
 		for (field fi: f)
 		dt.add(fi);
+	}
+	if (edge_path)
+	{
+		dt.convert();
+		dt.set_colour(edge_colour);
+		for (line ll : l)
+			dt.add(ll);
+		dt.set_colour(plan_colour);
 	}
 
 	return dt.to_string();
@@ -237,6 +257,8 @@ void print_usage()
 		cerr << " -N <number>       Limit number of Machina Blockers" << endl;
 		cerr << " -s                Add split fields" << endl;
 		cerr << " -C <#colour>      Set Drawtools output colour" << endl;
+		cerr << " -e                Draw cyclone edge" << endl;
+		cerr << " -c <#colour>      Cyclone edge path  colour" << endl;
 		cerr << " -L                Set Drawtools to output as polylines" << endl;
 		cerr << " -O                Output as Intel Link" << endl;
 		cerr << " -T <lat,lng,...>  Use only fields covering target points" << endl;
@@ -249,6 +271,7 @@ int main (int argc, char* argv[])
 	vector<point>target;
 	int calc = 0;  // area or mu
 	int same_size = 0; // same calc
+	bool drawedge = false;
 
 	arguments ag(argc,argv);
 
@@ -256,6 +279,8 @@ int main (int argc, char* argv[])
 	ag.add_req("R","resistance",true); // max resistance blockers
 	ag.add_req("N","machina",true); // max machina blockers
 	
+	ag.add_req("e","edges",false); // show cyclone edge path
+	ag.add_req("c","edgecolour",true); // cyclone edge path colour
 	ag.add_req("C","colour",true); // drawtools colour
 	ag.add_req("O","polylines",false); // output as polylines
 	ag.add_req("L","intel",false); // output as intel
@@ -278,10 +303,23 @@ int main (int argc, char* argv[])
 
 	team_count tc = team_count(ag.get_option_for_key("E"),ag.get_option_for_key("R"),ag.get_option_for_key("N"));
 	draw_tools dt;
+	draw_tools edt;
 
 	if (ag.has_option("C")) {
 		cerr << "set colour: " << ag.get_option_for_key("C") << endl;
 		dt.set_colour(ag.get_option_for_key("C"));
+	}
+
+	if (ag.has_option("e"))
+		drawedge = true;
+
+	edt.set_colour("#5db53d");
+	if (ag.has_option("c"))
+	{
+		if(drawedge)
+			edt.set_colour(ag.get_option_for_key("c"));
+		else
+			cerr << "Warning: setting edge colour but not drawing edges." << endl;
 	}
 
 	if (ag.has_option("L"))
@@ -444,7 +482,7 @@ int main (int argc, char* argv[])
 
 	vector<field> search;
 		//search.push_back(tfi);
-	cyclonefields mf = cyclonefields(dt,rt,calc,enable_splits,all_fields);
+	cyclonefields mf = cyclonefields(dt,edt,rt,calc,enable_splits,drawedge,all_fields);
 	mf.search_fields();
 	//search_fields(dt,search,all_fields,0,0,calc,same_size,0.0,rt);
 
